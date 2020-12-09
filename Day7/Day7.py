f687b230797a078347b7940c6f8fe837cab6f818
Leisha Burford <lburford@brocksolutions.com>
2020-12-09 15:42:20 -0500

    this isn't going to work

--- Day7/Day7.py
+++ Day7/Day7.py
@@ -4,23 +4,31 @@ from re import compile
 class Bag:
     def __init__(self, regex, rule):
         matches = [match for match in regex.findall(rule)[0] if match and match != ' ']
-        self.colour = matches[0]
-        self.contents = []
+        self.colour = matches[0].strip()
+        self.contents = {}
         for i in range(1, len(matches) - 1, 2):
-            self.contents.append((int(matches[i].strip()), matches[i + 1].strip()))
+            self.contents[matches[i + 1].strip()] = int(matches[i].strip())
 
     def __str__(self):
-        return self.colour + ": " + " ".join([str(a) + "-" + b for a,b in self.contents if a != 'no other'])
+        return self.colour + ": " + " ".join([a + "x " + str(b) for a,b in self.contents.items() if b != 'no other'])
     def __repr__(self):
         return str(self)
     def can_carry_bag(self, bag_colour):
-        can_carry = [i for i, v in enumerate(self.contents) if v[1] == bag_colour and v[0] > 0]
-        if len(can_carry) > 0:
-            return True
-        return False
+        return bag_colour in self.contents
 bag_regulations = [rule.strip() for rule in open('Day7/bag_regulations.txt').readlines()]
 bag_rules_regex = compile(r'([\w| ]+)bags contain (?:(\d+|no other)([\w| ]+)bags*)[, ]*(?:(\d+|no other)([\w| ]+)bags*)*')
 bags = [Bag(bag_rules_regex, rule) for rule in bag_regulations]
 can_carry = [bag for bag in bags if bag.can_carry_bag('shiny gold')]
+
+keep_checking = True
+while(keep_checking):
+    keep_checking = False
+    for bag in bags:
+        for innerbag in bag.contents.keys():
+            can_carry_colours = [carry.colour for carry in can_carry]
+            if innerbag in can_carry_colours:
+                can_carry.append(bag)
+                keep_checking = True
+                continue
 for bag in can_carry:
     print(bag)
