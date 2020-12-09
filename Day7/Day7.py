e7b89e94b22e3614b58e32191cdad1c7066f1006
Leisha Burford <leisha@Leishas-MacBook-Pro.local>
2020-12-09 08:19:10 -0500

    more day7

--- Day7/Day7.py
+++ Day7/Day7.py
@@ -14,11 +14,13 @@ class Bag:
     def __repr__(self):
         return str(self)
     def can_carry_bag(self, bag_colour):
-        if len([i for i, v in enumerate(se;f.contents) if v[0] == bag_colour]) > 1:
+        can_carry = [i for i, v in enumerate(self.contents) if v[1] == bag_colour and v[0] > 0]
+        if len(can_carry) > 0:
             return True
         return False
 bag_regulations = [rule.strip() for rule in open('Day7/bag_regulations.txt').readlines()]
 bag_rules_regex = compile(r'([\w| ]+)bags contain (?:(\d+|no other)([\w| ]+)bags*)[, ]*(?:(\d+|no other)([\w| ]+)bags*)*')
 bags = [Bag(bag_rules_regex, rule) for rule in bag_regulations]
-for bag in bags:
+can_carry = [bag for bag in bags if bag.can_carry_bag('shiny gold')]
+for bag in can_carry:
     print(bag)
