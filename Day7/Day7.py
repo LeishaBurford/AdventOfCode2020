57e93d5987299ad4bb9dcacdd2afed0fae695a67
Leisha Burford <leisha@Leishas-MacBook-Pro.local>
2020-12-09 21:19:27 -0500

    maybe

--- Day7/Day7.py
+++ Day7/Day7.py
@@ -1,34 +1,30 @@
 from pathlib import Path
 from re import compile
+from collections import deque
 
-class Bag:
-    def __init__(self, regex, rule):
-        matches = [match for match in regex.findall(rule)[0] if match and match != ' ']
-        self.colour = matches[0].strip()
-        self.contents = {}
+def build_bag_dict( rules):
+    bag_rules_regex = compile(r'([\w| ]+)bags contain (?:(\d+|no other)([\w| ]+)bags*)[, ]*(?:(\d+|no other)([\w| ]+)bags*)*')
+    bag_dict = {}
+    for rule in rules:
+        matches = [match for match in bag_rules_regex.findall(rule)[0] if match and match != ' ']
+        bag_dict[matches[0].strip()] = []
         for i in range(1, len(matches) - 1, 2):
-            self.contents[matches[i + 1].strip()] = int(matches[i].strip())
+            bag_dict[matches[0].strip()].append(([matches[i + 1].strip()], int(matches[i].strip())))
+    return bag_dict
 
-    def __str__(self):
-        return self.colour + ": " + " ".join([a + "x " + str(b) for a,b in self.contents.items() if b != 'no other'])
-    def __repr__(self):
-        return str(self)
-    def can_carry_bag(self, bag_colour):
-        return bag_colour in self.contents
 bag_regulations = [rule.strip() for rule in open('Day7/bag_regulations.txt').readlines()]
-bag_rules_regex = compile(r'([\w| ]+)bags contain (?:(\d+|no other)([\w| ]+)bags*)[, ]*(?:(\d+|no other)([\w| ]+)bags*)*')
-bags = [Bag(bag_rules_regex, rule) for rule in bag_regulations]
-can_carry = [bag for bag in bags if bag.can_carry_bag('shiny gold')]
 
-keep_checking = True
-while(keep_checking):
-    keep_checking = False
-    for bag in bags:
-        for innerbag in bag.contents.keys():
-            can_carry_colours = [carry.colour for carry in can_carry]
-            if innerbag in can_carry_colours:
-                can_carry.append(bag)
-                keep_checking = True
-                continue
-for bag in can_carry:
+direct_contents = build_bag_dict(bag_regulations)
+all_contents = dict.fromkeys(direct_contents.keys(), [])
+
+
+for colour,contents in direct_contents.items():
+    q = deque(contents)
+    while len(q) > 0:
+        bag = q.popleft()
+        all_contents[colour].append(bag)
+        q.append(bag)
+
+for bag in all_contents:
     print(bag)
+# print(build_bag_dict(bag_regulations))
