61aee7bddaf592e837d524d0ef998147bc0764a5
Leisha Burford <leisha@Leishas-MacBook-Pro.local>
2020-12-16 08:33:09 -0500

    next attempt

--- Day10/Day10.py
+++ Day10/Day10.py
@@ -34,33 +34,50 @@ def has_a_chance(adapters):
                 return False
     return True
 
+# def powerset(iterable):
+#     s = list(iterable)
+#     valid_arrangements = []
+#     for r in range(1, len(s) + 1):
+#         arrangements = list(combinations(s, r))
+#         for combination in arrangements:
+#             if has_a_chance(list(combination)):
+#                 valid_arrangements.append(list(combination))
+#     return valid_arrangements
+    # return chain.from_iterable(combinations(s, r) for r in range(1, len(s)+1) if has_a_chance(list(combinations(s, r))))
 def powerset(iterable):
     s = list(iterable)
-    valid_arrangements = []
-    for r in range(1, len(s) + 1):
-        arrangements = list(combinations(s, r))
-        for combination in arrangements:
-            if has_a_chance(list(combination)):
-                valid_arrangements.append(list(combination))
-    return valid_arrangements
-    # return chain.from_iterable(combinations(s, r) for r in range(1, len(s)+1) if has_a_chance(list(combinations(s, r))))
+    return chain.from_iterable(combinations(s, r) for r in range(1, len(s)+1))
 
 diff_dict = get_joltage_diffs(adapters)
 print("Part 1:", diff_dict[1] * diff_dict[3])
 
+
+
+
+# memo = {0: 1}
+# for r in adapters:
+#     memo[r] = memo.get(r-3,0) \
+#           + memo.get(r-2,0) \
+#           + memo.get(r-1,0)
+#     print(r, memo)
+# print(memo[adapters[-1]])
+
+
 built_in_adapter = max(adapters)
-
-
-memo = {0: 1}
-for r in adapters:
-    memo[r] = memo.get(r-3,0) \
-          + memo.get(r-2,0) \
-          + memo.get(r-1,0)
-    print(r, memo)
-print(memo[adapters[-1]])
-
 # adapters.remove(max(adapters))
+adapters.append(0)
+adapters.sort()
+required_adapters = dict.fromkeys(adapters, False)
+required_adapters[built_in_adapter] = True
+required_adapters[0] = True
 
+for index in range(1, len(adapters) - 1):
+    if abs(adapters[index - 1] - adapters[index]) >= 3 or abs(adapters[index + 1] - adapters[index]) >= 3:
+        required_adapters[adapters[index]] = True
+
+powerset_not_required = [list(combo) for combo in list(powerset([adapter for adapter,is_required in required_adapters.items() if not is_required]))]
+print(len(powerset_not_required))
+# print(required_adapters)
 # adapter_powerset = [list(arrangement) for arrangement in list(powerset(adapters))]
 # # print(len(adapter_powerset))
 # valid_arrangements = 0
